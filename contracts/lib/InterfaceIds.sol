077590626712adadcb1827e73013d0c280df66c7
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-11-03 16:44:23 +0700

    InterfaceIds I_TETU_CONVERTER

--- contracts/lib/InterfaceIds.sol
+++ contracts/lib/InterfaceIds.sol
@@ -32,5 +32,6 @@ library InterfaceIds {
   bytes4 public constant I_PLATFORM_VOTER = bytes4(keccak256('IPlatformVoter'));
   bytes4 public constant I_VE_DISTRIBUTOR = bytes4(keccak256('IVeDistributor'));
   bytes4 public constant I_VAULT_INSURANCE = bytes4(keccak256('IVaultInsurance'));
+  bytes4 public constant I_TETU_CONVERTER = bytes4(keccak256('ITetuConverter'));
 
 }
