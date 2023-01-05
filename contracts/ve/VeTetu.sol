13060c4c3e2ad9274dd520ddf34ad51ebe971fb6
dv <dvpublic0@gmail.com>
2023-01-05 13:29:19 +0700

    remove unnecessary comment

--- contracts/ve/VeTetu.sol
+++ contracts/ve/VeTetu.sol
@@ -490,7 +490,6 @@ contract VeTetu is ControllableV3, ReentrancyGuard, IERC721, IERC721Metadata, IV
   }
 
   /// @dev Add a NFT to a given address
-  ///      Throws if `_tokenId` is owned by someone.    // todo This function doesn't throw.. there is assumption below and there is no require(..). Remove the comment(?)
   function _addTokenTo(address _to, uint _tokenId) internal {
     // assume always call on new tokenId or after _removeTokenFrom() call
     // Change the owner
