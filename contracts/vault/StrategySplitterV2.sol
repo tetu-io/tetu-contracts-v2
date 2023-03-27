c81c5accb38964d826a6682cada48858b4448c87
Alien Deployer <m@a17.name>
2023-03-27 20:01:25 +0300

    splitter bugfix

--- contracts/vault/StrategySplitterV2.sol
+++ contracts/vault/StrategySplitterV2.sol
@@ -24,7 +24,7 @@ contract StrategySplitterV2 is ControllableV3, ReentrancyGuard, ISplitter {
   // *********************************************
 
   /// @dev Version of this contract. Adjust manually on each code modification.
-  string public constant SPLITTER_VERSION = "2.0.3";
+  string public constant SPLITTER_VERSION = "2.0.4";
   /// @dev APR denominator. Represent 100% APR.
   uint public constant APR_DENOMINATOR = 100_000;
   /// @dev Delay between hardwork calls for a strategy.
@@ -727,6 +727,10 @@ contract StrategySplitterV2 is ControllableV3, ReentrancyGuard, ISplitter {
         IERC20(_asset).safeTransfer(strategy, toInvest);
         totalAssetsDelta = IStrategyV2(strategy).investAll(toInvest, updateTotalAssetsBeforeInvest);
         emit Invested(strategy, toInvest);
+        balance = IERC20(_asset).balanceOf(address(this));
+        if (balance == 0) {
+          break;
+        }
       }
     }
 
