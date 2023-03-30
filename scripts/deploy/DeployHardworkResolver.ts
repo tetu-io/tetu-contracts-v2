a7c94793d83db8663cf45ccaf5e8e95a947bd94d
belbix <7453635@gmail.com>
2023-03-30 10:18:43 +0300

    deploy hardwork resolver

---
+++ scripts/deploy/DeployHardworkResolver.ts
@@ -0,0 +1,21 @@
+import {ethers} from "hardhat";
+import {DeployerUtils} from "../utils/DeployerUtils";
+import {appendFileSync} from "fs";
+import {Addresses} from "../addresses/addresses";
+import {HardWorkResolver__factory} from "../../typechain";
+import {RunHelper} from "../utils/RunHelper";
+
+
+async function main() {
+  const signer = (await ethers.getSigners())[0];
+  const core = Addresses.getCore();
+  const contract = await DeployerUtils.deployProxy(signer, 'HardWorkResolver');
+  await RunHelper.runAndWait(() => HardWorkResolver__factory.connect(contract, signer).init(core.controller));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
