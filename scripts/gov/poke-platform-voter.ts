a2187c86e6557d3c811c567bb5a2c55d34f16e2f
belbix <7453635@gmail.com>
2023-07-19 13:13:30 +0300

    fix dependencies

--- scripts/gov/poke-platform-voter.ts
+++ scripts/gov/poke-platform-voter.ts
@@ -1,8 +1,10 @@
 import {ethers} from "hardhat";
 import {Addresses} from "../addresses/addresses";
 import {PlatformVoter__factory} from "../../typechain";
-import {createClient} from "urql";
 import {RunHelper} from "../utils/RunHelper";
+
+// tslint:disable-next-line:no-var-requires
+const {request, gql} = require('graphql-request')
 
 async function main() {
   const [signer] = await ethers.getSigners();
@@ -10,35 +12,27 @@ async function main() {
 
   const voter = PlatformVoter__factory.connect(core.platformVoter, signer);
 
-  const client = createClient({
-    url: 'https://api.thegraph.com/subgraphs/name/tetu-io/tetu-v2',
-  })
-
-  const query = `query {
-  platformVoterEntities {
-    votes(first: 1000) {
-      desiredValue
-      date
-      newValue
-      percent
-      target
-      voteType
-      veWeightedValue
-      vePower
-      veNFT {
-        veNFTId
+  const data = await request('https://api.thegraph.com/subgraphs/name/tetu-io/tetu-v2', gql`
+      query {
+          platformVoterEntities {
+              votes(first: 1000) {
+                  desiredValue
+                  date
+                  newValue
+                  percent
+                  target
+                  voteType
+                  veWeightedValue
+                  vePower
+                  veNFT {
+                      veNFTId
+                  }
+              }
+          }
       }
-    }
-  }
-  }`;
+  `);
 
-  const data = await client.query(query, {}).toPromise()
-  if (data.error) {
-    throw data.error
-  }
-
-  const votes = data.data.platformVoterEntities[0].votes;
-  // console.log(votes)
+  const votes = data.platformVoterEntities[0].votes;
 
   const minDate = new Map<number, number>()
   for (const vote of votes) {
@@ -64,7 +58,7 @@ async function main() {
 
   for (const veId of vePokes) {
     console.log('poke ve: ', veId);
-    await RunHelper.runAndWait(() => voter.poke(veId));
+    // await RunHelper.runAndWait(() => voter.poke(veId));
   }
 
 }
